3599a2b38bdd079148db44ea8d92d2c3233af962
João Vitor Moreira <themoreira.jvm@gmail.com>
2021-03-28 17:14:13 -0300

    login page and vuex store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,68 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default new Vuex.Store<StoreInterface>({
   state: {
+    authorizationToken: '',
+    user: null,
+  },
+  getters: {
+    getAuthorizationToken(state) { return state.authorizationToken; },
+    getUser(state) { return state.user; },
+    userLoggedIn(state) { return state.user !== null; },
   },
   mutations: {
+    SET_AUTHORIZATION_TOKEN(state, token: string) {
+      state.authorizationToken = token;
+      localStorage.setItem('Authorization', token);
+      window.axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+    },
+    SET_USER(state, user: UserInterface) {
+      state.user = user;
+      localStorage.setItem('user', JSON.stringify(user));
+    },
+    LOGOUT(state) {
+      state.user = null;
+      state.authorizationToken = null;
+      localStorage.clear()
+    },
   },
   actions: {
+    apiGetRequest({ dispatch }, path: string) {
+      return window.axios.get(path)
+        .then(response => response.data)
+        .catch(error => dispatch('handleError', error));
+    },
+    apiPostRequest({ dispatch }, { path, payload, options = {} }) {
+      return window.axios.post(path, payload, options)
+        .then(response => response.data)
+        .catch(error => dispatch('handleError', error));
+    },
+    handleError({ dispatch }, error) {
+      const responseData = error.response && error.response.data;
+      return Promise.reject(responseData || error);
+    },
+    async signInAndStoreToken({ commit }, { email, password }: { email: string, password: string }) {
+      const { user } = await firebase.auth().signInWithEmailAndPassword(email, password);
+      if(!user)
+        return;
+
+      const idToken = await user.getIdToken();
+      commit('SET_AUTHORIZATION_TOKEN', idToken);
+      commit('SET_USER', { email: user.email, displayName: user.displayName });
+    }
   },
-  modules: {
-  }
 })
+
+interface StoreInterface {
+  authorizationToken: string | null,
+  user: UserInterface | null,
+}
+
+interface UserInterface {
+  displayName: string,
+  email: string,
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-    <h1>Login Page</h1>
-  </div>
+  <v-container fluid>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-form @submit.prevent="login" lazy-validation ref="formRef">
+          <v-card class="elevation-12">
+            <v-toolbar dark>
+              <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                v-model="email"
+                name="email"
+                :rules="!formSent ? [] : [this.requiredFieldRule]"
+                label="Email"
+                type="text"
+                inputmode="email"
+                required
+                color="secondary"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                name="password"
+                :rules="!formSent ? [] : [this.requiredFieldRule, this.minLengthRule(6)]"
+                label="Password"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPass = !showPass"
+                required
+                color="secondary"
+                :type="showPass ? 'text' : 'password'"
+              ></v-text-field>
+              <span v-if="errorMessage" class="error--text mt-3">{{ errorMessage }}</span>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn type="submit" dark>Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    name: 'Home',
+    data: () => ({
+      formSent: false,
+      email: '',
+      password: '',
+      showPass: false,
+      errorMessage: '',
+    }),
+    computed: {
+    },
+    methods: {
+      async login() {
+        this.formSent = true;
+        if(!this.email || !this.password)
+          return;
+        try {
+          await this.$store.dispatch('signInAndStoreToken', { email: this.email, password: this.password});
+          await this.$router.push({ path: this.$route.query.redirectTo as string || '/about' });
+        } catch (error) {
+          this.errorMessage = error.message;
+        }
+      },
+      requiredFieldRule(value: string) {
+        return !value ? 'This field is required' : true;
+      },
+      minLengthRule(minLength: number) {
+        return function(value: string) {
+          return value.length < minLength ? `Password must have at least ${minLength} characters` : true;
+        };
+      },
+    },
+  });
+</script>
